572e975992927a308698ef1afee051353a665fcc
Thomas Neuenschwader <130864240+thomneuenschwander@users.noreply.github.com>
2024-03-20 14:23:20 -0300

    Noels Label C

---
+++ exercicios/beecrownd/categoria03/NoelsLabel.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Noel's Labels
+ * 
+ *  link -> https://judge.beecrowd.com/en/problems/view/2482
+ * 
+ * 
+ *  Análise assintótica:
+ * 
+ *     
+ *     O(N) -> Para percorres as traduções / mapear
+ * 
+ *     Total -> O(N)
+*/
+
+typedef struct TranslationMapper {
+    char *language;
+    char *greeting;
+}Mapper;
+
+
+char* mapCountryToGreeting(Mapper* translations, int N, char *country){
+    for(int i = 0; i < N; i++)
+    {
+        if(strcmp(country, translations[i].language) == 0){
+            return translations[i].greeting;
+        }
+    }
+    return NULL;
+}
+
+void readTranslations(Mapper* translations, int N);
+
+void freeTranslations(Mapper* translations, int N);
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    Mapper translations[N];
+    readTranslations(translations, N);
+    
+    int M;
+    scanf("%d", &M);
+
+    while (M--)
+    {
+        char name[31];
+        char country[31];
+
+        scanf(" %[^\n]", name);
+        scanf("%s", country);
+
+        char* greeting = mapCountryToGreeting(translations, N, country);
+
+        printf("%s\n", name);
+        printf("%s\n", greeting);
+    }
+    freeTranslations(translations, N);
+    return 0;
+}
+
+void readTranslations(Mapper* translations, int N){
+    for(int i = 0; i < N; i++)
+    {   
+        char language[31], greeting[31];
+        scanf("%s", language);
+        scanf(" %[^\n]", greeting);
+
+        translations[i].language = malloc(31*sizeof(char));
+        strcpy(translations[i].language, language);
+
+        translations[i].greeting = malloc(31*sizeof(char));
+        strcpy(translations[i].greeting, greeting);
+    }
+}
+
+void freeTranslations(Mapper* translations, int N) {
+    for (int i = 0; i < N; i++) {
+        free(translations[i].language);
+        free(translations[i].greeting);
+    }
+}
